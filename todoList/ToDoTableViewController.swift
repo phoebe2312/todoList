71ad21c67cf628b2c6f20fc5cac5bc371ce61399
Phoebe Chen <kexing42@gmail.com>
2022-07-19 13:42:07 -0400

    Added iteration 1

--- todoList/ToDoTableViewController.swift
+++ todoList/ToDoTableViewController.swift
@@ -43,7 +43,6 @@ class ToDoTableViewController: UITableViewController {
         return toDos.count
         
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
@@ -57,6 +56,14 @@ class ToDoTableViewController: UITableViewController {
           }
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+      // this gives us a single ToDo
+      let toDo = toDos[indexPath.row]
+
+      performSegue(withIdentifier: "moveToComplete", sender: toDo)
     }
 
     /*
@@ -103,6 +110,13 @@ class ToDoTableViewController: UITableViewController {
         if let addVC = segue.destination as? AddToDoViewController {
             addVC.previousVC = self
           }
+        
+        if let completeVC = segue.destination as? CompleteToDoViewController {
+          if let toDo = sender as? ToDo {
+            completeVC.selectedToDo = toDo
+            completeVC.previousVC = self
+          }
+        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
